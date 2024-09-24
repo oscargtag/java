f5ffc37f41e2a98e46725df68c377489a57d291e
Sevelte <btdaws.28@jacksboroisd.net>
2024-09-24 19:43:55 +0000

    initials

---
+++ Initials.java
@@ -0,0 +1,12 @@
+public class Initials {
+    public static void main(String[] args) {
+        // initial B
+        System.out.println("BBBBB ");
+        System.out.println("B    B");
+        System.out.println("B    B");
+        System.out.println("BBBBB ");
+        System.out.println("B    B");
+        System.out.println("B    B");
+        System.out.println("BBBBB ");
+    }
+}
